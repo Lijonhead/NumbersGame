025a4fc5734f2f73bf8756acf91c7d8565cf0257
Hugo Leijonhufvud <thechosenwolf@outlook.com>
2023-10-01 19:51:45 +0200

    Add project files.

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace NumbersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök. ");
+            Random random = new Random();
+            int randomNumber = random.Next(0, 21);
+
+            string input = "";
+            int number = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                input = Console.ReadLine();
+                number = int.Parse(input);
+                Console.WriteLine($"Försök {i}: {number}");
+                if (number == randomNumber)
+                {
+                    Console.WriteLine("Wohoo! Du klarade det!");
+                    break;
+                }
+                else if (i != 5)
+                {
+                    
+                    checkguess(number, randomNumber);
+
+                }
+                else
+                {
+                    Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök!");
+
+                }
+            }
+
+
+        }
+
+
+
+        static void checkguess(int guess, int rando)
+        {
+
+            if (guess > rando)
+            {
+                Console.WriteLine("Tyvärr, du gissade för högt! ");
+
+            }
+
+
+            else
+            {
+                Console.WriteLine("Tyvärr, du gissade för lågt!");
+            }
+
+
+        }
+
+
+
+
+    }
+}
